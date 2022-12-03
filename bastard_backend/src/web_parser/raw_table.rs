2f271ef68e2b293c7879f40616728327742594f7
User Admin <schumov.nn@gmail.com>
2022-12-03 20:10:53 +0500

    Improved table generator

--- bastard_backend/src/web_parser/raw_table.rs
+++ bastard_backend/src/web_parser/raw_table.rs
@@ -19,47 +19,66 @@ pub struct RawTable {
 
 impl RawTable {
     pub fn to_table(&self) -> ScheduleTable {
-        let new = &self.lessons;
-
         let mut table = ScheduleTable::new();
-        for i in 1..=7 {
-            let entry = new.iter().filter(|x| x.number == i).map(|x| x.clone()).collect::<Vec<RawLesson>>(); 
-            let count = entry.len();
+
+        if self.lessons.len() == 0 {
+            return table;
+        }
+        let min_place: i32 = self.lessons.iter().min_by(|a, b| a.number.cmp(&b.number)).unwrap().number;
+        let max_place: i32 = self.lessons.iter().max_by(|a, b| a.number.cmp(&b.number)).unwrap().number;
+        for place in min_place..=max_place {
+            let lessons: Vec<&RawLesson> = self.lessons
+                .iter()
+                .filter(|x| x.number == place)
+                .collect();
+
+            let count = lessons.len();
+            
             let entry = match count {
-                0 => {
-                    Entry::NoLessons
-                },
+                0 => Entry::NoLessons,
                 1 => {
-                    let one = entry[0].clone();
+                    let lesson = lessons[0];
                     let one = Lesson {
-                        name: one.subject,
-                        teacher: one.teacher,
-                        auditory: one.auditory,
+                        name: lesson.subject.clone(),
+                        teacher: lesson.teacher.clone(),
+                        auditory: lesson.auditory.clone(),
                         replacing: None,
                     };
-                    Entry::OneLesson(one)
+                    match lesson.subgroup {
+                        1 => Entry::TwoLessons(Some(one), None),
+                        2 => Entry::TwoLessons(None, Some(one)),
+                        _ => Entry::OneLesson(one),
+                    }
                 },
                 2 => {
-                    let one = entry[0].clone();
+                    let lesson = lessons[0];
                     let one = Lesson {
-                        name: one.subject,
-                        teacher: one.teacher,
-                        auditory: one.auditory,
+                        name: lesson.subject.clone(),
+                        teacher: lesson.teacher.clone(),
+                        auditory: lesson.auditory.clone(),
                         replacing: None,
                     };
-                    let two = entry[1].clone();
+
+                    let lesson = lessons[1];
                     let two = Lesson {
-                        name: two.subject,
-                        teacher: two.teacher,
-                        auditory: two.auditory,
+                        name: lesson.subject.clone(),
+                        teacher: lesson.teacher.clone(),
+                        auditory: lesson.auditory.clone(),
                         replacing: None,
                     };
-                    Entry::TwoLessons(Some(one), Some(two))
+
+                    if lessons[0].subgroup > lessons[1].subgroup {
+                        Entry::TwoLessons(Some(two), Some(one))
+                    } else {
+                        Entry::TwoLessons(Some(one), Some(two))
+                    }
                 },
                 _ => unreachable!(),
             };
+
             table.add_entry(entry);
         }
+
         return table;
     }
 }
